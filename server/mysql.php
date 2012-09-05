25194e275c016544bcb0339b40de391da2d8a3bf
malaDev <malagroup.systems@gmail.com>
2012-09-05 03:13:15 +0300

    Update server/mysql.php
    just mysql

--- server/mysql.php
+++ server/mysql.php
@@ -2,7 +2,7 @@
 
 $host="127.0.0.1";
 $user="root";
-$pass="pass";
+$pass="mysql";
 $base="carte";
 $connexion = mysql_connect($host,$user,$pass) or die ("Erreur d'accès aux informations");
 $db=mysql_select_db($base,$connexion)or die ("Erreur de connexion à la base de données");
